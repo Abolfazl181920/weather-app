8d56f63cfbd24eefbf63690f6de6b368424f93ed
Abolfazl <abolfazlm181920@gmail.com>
2022-01-02 20:11:12 +0330

    delete App and index csses

--- src/App.js
+++ src/App.js
@@ -10,11 +10,10 @@ const App = () => {
   return(
     <div className={darkTheme ? 'dark' : ''}>
       <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
-        <Navbar />
+        <Navbar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
         <Routes />
         <Footer />
       </div>
-      <p>lorem</p>
     </div>
   );
 }
