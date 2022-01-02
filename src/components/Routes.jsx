f4c2e64b9479f9f4939a50393fd29412c6ac4c1b
Abolfazl <abolfazlm181920@gmail.com>
2022-01-02 20:25:50 +0330

    add Footer component codes and styles

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -6,14 +6,14 @@ import { Results } from './Results';
 export const Routes = () => {
     return (
         <div className="p-4">
-            <Switch>
+            {/* <Switch>
                 <Route exact path="/">
                     <Redirect to="/search" />
                 </Route>
                 <Route exact path={['/search', '/images', '/news', '/videos']}>
                     <Results />                    
                 </Route>
-            </Switch>
+            </Switch> */}
         </div>
     )
 }
