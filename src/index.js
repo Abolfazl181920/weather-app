cbe52de76f9672ffb1048aee1f02181259768be5
Abolfazl <abolfazlm181920@gmail.com>
2022-01-04 16:36:07 +0330

    install all packages

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
-
-
-// ReactDOM.render(
-//   <Router>
-//     <React.StrictMode>
-//       <App />
-//     </React.StrictMode>,
-//   </Router>,
-//   document.getElementById('root')
-// );
+import './global.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
