059caaa01052317bc3c40d1cb93b07940c1dc836
Abolfazl <abolfazlm181920@gmail.com>
2022-01-12 22:34:56 +0330

    delete junk codes and delete styles

--- src/App.js
+++ src/App.js
@@ -4,33 +4,34 @@ import Weather from './Weather';
 
 const App = () => {
 
-  const [ lat, setLat ] = useState(['50']);
-  const [ long, setLong ] = useState(['8']);
-  const [ data, setData ] = useState([]);
+    const [ lat, setLat ] = useState(['35.6892']);
+    const [ long, setLong ] = useState(['51.3890']);
+    const [ data, setData ] = useState([]);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
-        .then(res => res.json())
-        .then(result => {
-        setData(result)
-        console.log(result);
-      });
-    }
-    fetchData();
-  }, [lat,long])
+    useEffect(() => {
+        const fetchData = async () => {
+            await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+            .then(res => res.json())
+            .then(result => {
+                setData(result);
+                console.log(result);
+            });
+        }
+        fetchData();
 
-  return(
-    <div className="App">
-      {
-        (typeof data.main != 'undefined') ? (
-          <Weather weatherData={data} />
-        ) : (
-          <div></div>
-        )
-      }
-    </div>
-  );
+    }, [ lat, long ]);
+
+    return(
+        <div className="App">
+            {
+                (typeof data.main != 'undefined') ? (
+                    <Weather weatherData={data} />
+                ) : (
+                    <></>
+                )
+            }
+        </div>
+    );
 }
 
 export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -2,26 +2,18 @@ import React from 'react';
 
 import moment from 'moment';
 
-const refresh = () => {
-    window.location.reload();
-}
-
-const CardExampleCard = ( {weatherData} ) => (
+const CardExampleCard = ({ weatherData }) => (
     <div className="main">
-      <p className="header">{weatherData.name}</p>
+        <p className="header"> {weatherData.name} </p>
+        
         <div className="flex">
-            <p className="day">Day: {moment().format('dddd')}</p>
-            <p className="day">{moment().format('LL')}</p>
+            <p className="day">Day: {moment().format('dddd')} </p>
+            <p className="day"> {moment().format('LL')} </p>
         </div>
 
         <div className="flex">
-            <p className="temp">Temprature: {weatherData.main.temp} &deg;C</p>
-            <p className="temp">Humidity: {weatherData.main.humidity} %</p>
-        </div>
-
-        <div className="flex">
-            <p className="sunrise-sunset">Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
-            <p className="sunrise-sunset">Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+            <p className="temp">Temprature: {weatherData.main.temp} &deg;C </p>
+            <p className="temp">Humidity: {weatherData.main.humidity} % </p>
         </div>
     </div>
 )
